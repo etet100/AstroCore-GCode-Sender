48bef2b4506119898198bf7d8723f967ef9282ac
etet100 <etet100>
2025-11-10 21:38:24 +0100

    Unused getters

--- src/gpilot/ui/drawers/heightmapgriddrawer.h
+++ src/gpilot/ui/drawers/heightmapgriddrawer.h
@@ -13,30 +13,10 @@ class HeightMapGridDrawer : public ShaderDrawable
 public:
     HeightMapGridDrawer(Heightmap &model);
 
-    // QPointF gridSize() const;
-    // void setGridSize(const QPointF &gridSize);
-
-    // QRectF borderRect() const;
-    // void setBorderRect(const QRectF &borderRect);
-
-    // double zTop() const;
-    void setZTop(double zTop);
-
-    // double zBottom() const;
-    void setZBottom(double zBottom);
-
-    // QAbstractTableModel *model() const;
-    // void setModel(QAbstractTableModel *model);
-
 protected:
     bool updateData(GLPalette &palette) override;
 
 private:
-    // QPointF m_gridSize;
-    // QRectF m_borderRect;
-    double m_zTop;
-    double m_zBottom;
-    // QAbstractTableModel *m_model;
     Heightmap &m_model;
     void generateLines(int gridPointsY, double min, QPointF startPos, double gridStepX, VertexData vertex, double max, GLPalette &palette, int gridPointsX, double gridStepY);
     void generateTriangles(int gridPointsY, double min, QPointF startPos, double gridStepX, VertexData vertex, double max, GLPalette &palette, int gridPointsX, double gridStepY);
